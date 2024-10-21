caf40942022408a27bdbbd918ffa1cb8701aa852
viadinho!!! <146236941+fernandinnn@users.noreply.github.com>
2024-10-21 10:42:57 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,52 +1,52 @@
 criaCartao(
-    'Programação',
+    'Pecuária',
     'O que é pecuária??',
     'A pecuária é a atividade econômica que envolve a criação de animais para a produção de carne, leite, ovos, lã, entre outros produtos.'
 )
 
 criaCartao(
-    'Geografia',
+    'Pecuária',
     'Quais são os principais tipos de pecuária?',
     'Os principais tipos incluem a pecuária de corte (produção de carne), pecuária de leite (produção de leite e derivados), e pecuária de pequenos animais (como aves e suínos).?'
 )
 
 criaCartao(
-    'Programação',
+    'Pecuária',
     'Quais são os principais desafios da pecuária?',
     'Os desafios incluem doenças animais, manejo inadequado, flutuações de preços, questões ambientais e a necessidade de melhorar a eficiência produtiva.'
 )
 
 criaCartao(
-    'Lingua inglesa',
+    'Pecuária',
     'Como a tecnologia está impactando a pecuária?',
     'A tecnologia tem melhorado o manejo, a alimentação, a genética dos animais e a saúde, resultando em maior produtividade e sustentabilidade.'
 )
 criaCartao(
-    'Programação',
+    'Pecuária',
     'Qual a importância da alimentação na pecuária?',
     'A alimentação adequada é crucial para o crescimento, a reprodução e a produção de animais saudáveis, influenciando diretamente a qualidade e a quantidade dos produtos.'
 )
 
 criaCartao(
-    'Geografia',
+    'Pecuária',
     'O que é manejo sustentável na pecuária?',
     'Manejo sustentável envolve práticas que minimizam o impacto ambiental, conservam os recursos naturais e garantem o bem-estar animal, além de serem economicamente viáveis.'
 )
 
 criaCartao(
-    'Programação',
+    'Pecuária',
     'Quais são os principais produtos da pecuária?',
     'Os principais produtos incluem carne (bovina, suína, de frango), leite e seus derivados (queijo, iogurte), ovos, lã e couro.'
 )
 
 criaCartao(
-    'Geografia',
+    'Pecuária',
     'Como a pecuária pode afetar o meio ambiente?',
     'A pecuária pode contribuir para a degradação do solo, desmatamento, emissão de gases de efeito estufa e contaminação da água, se não for manejada de forma sustentável.'
 )
 
 criaCartao(
-    'Programação',
+    'Pecuária',
     'Quais são as práticas de bem-estar animal na pecuária?',
     'Práticas de bem-estar incluem proporcionar espaço adequado, alimentação balanceada, cuidados veterinários, manejo gentil e condições que permitam comportamentos naturais dos animais.'
 )
